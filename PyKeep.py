c904b7f301a36fb5f8e6466a4a6092ca291deb38
MengZe <59787257+sudo-MEnGzE@users.noreply.github.com>
2020-05-02 23:03:20 +0800

    Added Download features

--- PyKeep.py
+++ PyKeep.py
@@ -1,30 +1,45 @@
+'''
+PyKeep, version 1.0
+
+Keep your favourite Youtube videos in one app!
+
+(C) MengZe 2020-present
+'''
+
+# Copyright information.
+__author__ = 'MengZe'
+__copyright__ = '(C) MengZe 2020-present'
+__license__ = 'Public Domain'
+__version__ = '1.0.0'
 from pytube import YouTube
 import json
 import os
 import PySimpleGUI as sg
 
 # Create data.json
-format = {
+json_format = {
     "video":[]
 }
 
 open('data.json', 'a').close()
+open('data.txt', 'a').close()
 
-with open('data.json', 'r+') as database:
+
+with open('data.json', 'r+') as json_database:
     try:
-        json.load(database)
+        json.load(json_database)
     except:
-        json.dump(format, database, indent=4)
-
+        json.dump(json_format, json_database, indent=4)
 
 # Get video information and show on GUI
 class videoInfo:
+    global yt
     def __init__(self, url):
         self.url = url
         try:
             self.yt = YouTube(url)
         except:
-            sg.popup_error("Error", "Invalide URL")
+            sg.popup_annoying("Error", "Invalide URL")
 
     def getTitle(self):
         try:
@@ -50,8 +65,9 @@ class videoInfo:
         except:
             pass
 
-with open('data.json') as json_file: 
-    data = json.load(json_file) 
+
+with open('data.json') as json_database: 
+    data = json.load(json_database) 
 info = data['video']
 
 
@@ -81,7 +97,8 @@ class app:
             [sg.Listbox(values=[], size=(100, 8), enable_events=True, key='-Title-', pad=(5,10))],
             [sg.Button('Delete')],
             [sg.Multiline("URL: ", size=(100, 1), key='Info-1', pad=(5,10), right_click_menu= ['&Right', ['Copy']])],
-            [sg.Listbox(values=["Author: ", "View: ", "Rating: ", "Note: "], size=(100, 8), key='Info-2', pad=(5,0))]]
+            [sg.Listbox(values=["Author: ", "View: ", "Rating: ", "Note: "], size=(100, 8), key='Info-2', pad=(5,0))],
+            [sg.Button('Download Video', key='-Video-'), sg.Button('Download Audio', key='-Audio-')]]
 
 
     # Add video information to data.json
@@ -93,7 +110,7 @@ class app:
         videoRating = getVideoInfo.getRating()  
         # python object to be appended 
         if videoTitle:
-            y = {
+            json_info = {
                 "url": self.userInput,
                 "title": videoTitle,
                 "author": videoAuthor,
@@ -101,10 +118,13 @@ class app:
                 "rating": videoRating,
                 "note": self.note
             }
+            
+            with open('data.txt', 'a') as txt_database:
+                txt_database.write(self.userInput + "\n")
 
-            self.info.append(y)
-            with open('data.json','w') as f: 
-                json.dump(data, f, indent=4)
+            self.info.append(json_info)
+            with open('data.json','w') as json_database: 
+                json.dump(data, json_database, indent=4)
 
             for i in range(1,500):
                     sg.one_line_progress_meter('Progress Bar', i+1, 500, 'Uploading Information')
@@ -125,8 +145,8 @@ class app:
                 break
             x +=1
 
-        with open('data.json', 'w') as f:
-            json.dump(data, f, indent=4)
+        with open('data.json', 'w') as json_database:
+            json.dump(data, json_database, indent=4)
 
     # When the user click 'Keep', write the data to json and update the app with new data
     def recordInfo(self):
@@ -134,10 +154,10 @@ class app:
         self.note = values['-Note-']
         self.addInformation()
         self.window.FindElement('-Title-').Update(values=title)
-
+    
     # Run the window 
     def readWindow(self):
-        self.window = sg.Window("PyKeep", self.windowLayout, size=(700, 480), finalize=True)
+        self.window = sg.Window("PyKeep", self.windowLayout, size=(700, 500), finalize=True)
         self.window.FindElement('-Title-').Update(values=title)
         while True:
             global values
@@ -155,17 +175,40 @@ class app:
             elif event == '-Title-':
                 for x in range(0,len(info)):
                     if str(('["' + info[x]['title'] + '"]')) == str(values['-Title-']) or str(("['" + info[x]['title'] + "']")) == str(values['-Title-']):
-                        self.window.FindElement('Info-1').Update('URL:     ' + info[x]["url"])
+                        url = info[x]["url"]
+                        self.window.FindElement('Info-1').Update('URL:     ' + url)
                         self.window.FindElement('Info-2').Update(values= [('Author:  ' + info[x]["author"]), ('View:     ' + str(info[x]["view"])), ('Rating:   ' + str(info[x]["rating"])), ('Note:     ' + info[x]["note"])])
+
+            elif event == '-Audio-':
+                for x in range(0,len(info)):
+                    if str(('["' + info[x]['title'] + '"]')) == str(values['-Title-']) or str(("['" + info[x]['title'] + "']")) == str(values['-Title-']):
+                        url = info[x]["url"]
+                try:
+                    sg.popup_scrolled('This feature is not officially done yet','For a better experience, check out: ','https://github.com/jkelol111/tkyoutubedl','Click OK to continue')
+                    YouTube(url).streams.filter(only_audio=True).first().download()
+                    sg.popup_annoying('Download Successed')
+                except:
+                    sg.popup_annoying('Error', 'No video is selected')
+                
+            elif event == '-Video-':
+                for x in range(0,len(info)):
+                    if str(('["' + info[x]['title'] + '"]')) == str(values['-Title-']) or str(("['" + info[x]['title'] + "']")) == str(values['-Title-']):
+                        url = info[x]["url"]
+                try:
+                    sg.popup_scrolled('This feature is not officially done yet','For a better experience, check out: ','https://github.com/jkelol111/tkyoutubedl','Click OK to continue')
+                    YouTube(url).streams.first().download()
+                    sg.popup_annoying('Download Successed')
+                except:
+                    sg.popup_annoying('Error', 'No video is selected')
+
             elif event == 'About':
                 about= """
-About
-
 Welcome to PyKeep, an open source Python app that organize your Youtube Video Collection!
 
 Simply input the video url and wait for a few seconds. The information will automatically be uploaded to data.json database, and you can access your record anytime you want!
 """
-                sg.popup_scrolled(about, size=(50, 30), title='About')
+                sg.popup_scrolled(about, title='About')
+            print(event)
                 
 # Excute window
 pykeep = app()
